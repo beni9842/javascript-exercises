49dfbb6661a6f2f905b165943f78fc9bde39d813
Ben Niesmertelny <ben@res1-186-149-dhcp.int.colorado.edu>
2021-12-03 16:52:45 -0700

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  numbers.forEach(number => {
+    sum += number;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  numbers.forEach(number => {
+    product *= number;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	let product = 1;
+  for (let i = 0; i < e; i++) {
+    product *= b;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  
+  /* non-recursive solution
+
+  product = 1;
+  for (let i = 1; i <= x; i++) {
+    product *= i;
+  }
+  return product;
+
+  */
+
+  /* recursive solution */
+
+  if (x == 1 || x == 0) {
+    return 1;
+  } else {
+    return x * factorial(x-1);
+  }
 };
 
 // Do not edit below this line
