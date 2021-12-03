026507c3e518ccd89fa82fd96cb114e627ff402a
Ben Niesmertelny <ben@res1-186-149-dhcp.int.colorado.edu>
2021-12-03 14:43:33 -0700

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a == 'number' && typeof b == 'number') {
+        if (0 <= a && 0 < b) {
+            let sum = 0;
+            if (a > b) {
+                [a,b] = [b,a];
+            }
+            for (let i = a; i <= b; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            return 'ERROR';
+        }
+    } 
 
+    return 'ERROR';
 };
 
 // Do not edit below this line
