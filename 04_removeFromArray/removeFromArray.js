d570c5ba8b1afe416190870dc7217cb6cc804456
Ben Niesmertelny <ben@res1-186-149-dhcp.int.colorado.edu>
2021-12-03 14:32:17 -0700

    removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    let vals = Array.from(arguments).slice(1);
 
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (vals.indexOf(array[i]) < 0) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
