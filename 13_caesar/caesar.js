708e4b5499ca5fcd692934484bd48e4de015767f
Ben Niesmertelny <ben@res1-186-149-dhcp.int.colorado.edu>
2021-12-03 18:54:14 -0700

    caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,19 @@
-const caesar = function() {
 
+const caesar = function(message, key) {
+    cipher = "";
+    for (let i = 0; i < message.length; i++) {
+
+        let char = message.charCodeAt(i);
+        if (char >= 65 && char <= 90) {
+            char = (((char - 65 + key + 26 ) % 26) + 65);
+        } else if (char >= 97 && char <= 122) {
+            char = (((char - 97 + key + 26) % 26) + 97);
+        }
+    
+        cipher += String.fromCharCode(char)
+    }
+
+    return cipher;
 };
 
 // Do not edit below this line
