cb299ab57b6779c154f4e7ef00ff5c46e4f95f7f
Ben Niesmertelny <ben@res1-186-149-dhcp.int.colorado.edu>
2021-12-03 17:14:55 -0700

    findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest;
+    let age = 0;
+    people.forEach(person => {
+        let currAge;
+        
+        if (person.yearOfDeath) {
+            currAge = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            currAge = 2021 - person.yearOfBirth;
+        }
+        
+        if (currAge > age) {
+            age = currAge
+            oldest = person;
+        }
+    });
 
+    return oldest;
 };
 
 // Do not edit below this line
